20e7e1867c9c3faeb7c5785d7e7244b8febb5891
Florian von Stosch <florian.vonstosch@wps.de>
2019-03-05 20:49:07 +0100

    Sleep before ssh

--- resume-work.sh
+++ resume-work.sh
@@ -2,4 +2,5 @@ export ZONE="us-west2-b" # budget: "us-west1-b"
 export INSTANCE_NAME="my-fastai-instance"
 
 gcloud compute instances start $INSTANCE_NAME --zone=$ZONE
+sleep 10
 gcloud compute ssh --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
