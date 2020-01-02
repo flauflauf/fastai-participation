8be0e522014d5a087812c0d64784b4d5bd49f7ff
Florian von Stosch <florian.vonstosch@wps.de>
2020-01-02 12:19:03 +0100

    do not wait for stop command

--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 export ZONE="us-west2-b" # budget: "us-west1-b"
 export INSTANCE_NAME="my-fastai-instance"
 
-gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE
+gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE --async
