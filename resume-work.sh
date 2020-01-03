4d3cbb9ba762ed7a885eac17d4baa2541e7bfb07
Florian von Stosch <florian.vonstosch@wps.de>
2020-01-03 08:16:54 +0100

    fixed connection to new instance

--- resume-work.sh
+++ resume-work.sh
@@ -3,4 +3,4 @@ export INSTANCE_NAME="my-fastai-instance"
 
 gcloud compute instances start $INSTANCE_NAME --zone=$ZONE
 # sleep 10
-gcloud compute ssh --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
+gcloud beta compute ssh --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
