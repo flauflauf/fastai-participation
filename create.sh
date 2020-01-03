37c87083b43498aded1969f7883fd320eded3bb3
Florian von Stosch <florian.vonstosch@wps.de>
2020-01-03 08:08:38 +0100

    change instance (p4 -> p100, other zone)

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 export IMAGE_FAMILY="pytorch-latest-gpu" # or "pytorch-latest-cpu" for non-GPU instances
-export ZONE="us-west2-b" # budget: "us-west1-b"
+export ZONE="europe-west1-b"
 export INSTANCE_NAME="my-fastai-instance"
 export INSTANCE_TYPE="n1-highmem-8" # budget: "n1-highmem-4"
 
@@ -9,7 +9,7 @@ gcloud compute instances create $INSTANCE_NAME \
         --image-family=$IMAGE_FAMILY \
         --image-project=deeplearning-platform-release \
         --maintenance-policy=TERMINATE \
-        --accelerator="type=nvidia-tesla-p4,count=1" \
+        --accelerator="type=nvidia-tesla-p100,count=1" \
         --machine-type=$INSTANCE_TYPE \
         --boot-disk-size=200GB \
         --metadata="install-nvidia-driver=True" \
--- resume-work.sh
+++ resume-work.sh
@@ -1,4 +1,4 @@
-export ZONE="us-west2-b" # budget: "us-west1-b"
+export ZONE="europe-west1-b"
 export INSTANCE_NAME="my-fastai-instance"
 
 gcloud compute instances start $INSTANCE_NAME --zone=$ZONE
